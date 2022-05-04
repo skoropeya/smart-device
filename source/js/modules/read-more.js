56b36c2f42d8dc39916203a0943f2f1e441582b2
Kate <skoropeya@inbox.ru>
2022-05-04 21:53:21 +0300

    доработан скрипт для скрытого текста в мобильной версии

--- source/js/modules/read-more.js
+++ source/js/modules/read-more.js
@@ -1,22 +1,40 @@
 const btn = document.getElementById('btn-more-hide');
-const hiddenText = document.getElementById('hidden-text');
+const hiddenTextAll = document.getElementById('hidden-text-all');
+const hiddenTextMobile = document.getElementById('hidden-text-mobile');
+const mediaQuery = window.matchMedia('(max-width: 767px)');
 
-const checkBtn = () => {
+const openedBtn = () => {
   if (btn.dataset.isOpen === 'true') {
     return true;
   }
   return false;
 };
 
+const showText = () => {
+  if (mediaQuery.matches) {
+    hiddenTextMobile.style.display = 'inline';
+  }
+
+  hiddenTextAll.style.display = 'block';
+  btn.dataset.isOpen = true;
+};
+
+const hideText = () => {
+  if (mediaQuery.matches) {
+    hiddenTextMobile.style.display = 'none';
+  }
+
+  hiddenTextAll.style.display = 'none';
+  btn.dataset.isOpen = false;
+};
+
 const onClickReadMore = (evt) => {
   evt.preventDefault();
 
-  if (checkBtn()) {
-    hiddenText.style.display = 'none';
-    btn.dataset.isOpen = false;
+  if (openedBtn()) {
+    hideText();
   } else {
-    hiddenText.style.display = 'block';
-    btn.dataset.isOpen = true;
+    showText();
   }
 };
 
