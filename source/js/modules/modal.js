741b08812d81aa9cc85759dda325a4d1dcb6aa78
Kate <skoropeya@inbox.ru>
2022-05-18 11:01:59 +0300

    ловушка фокуса в модальном окне

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,12 +1,35 @@
 const modal = document.querySelector('.modal');
 const btnClose = modal.querySelector('#btn-close');
 const btnOpen = document.getElementById('request-call');
+let lastFocusedElement;
+
+const focusableElements = Array.from(modal.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]),  select:not([disabled]), [tabindex], audio[controls]:not([disabled]), video[controls]:not([disabled])'));
+
+const firstFocusableElem = focusableElements[0];
+const lastFocusableElem = focusableElements[focusableElements.length - 1];
 
 const keydownDocument = (evt) => {
   const isEscKey = evt.key === 'Escape' || evt.key === 'Esc';
   if (isEscKey) {
     evt.preventDefault();
     closeModal();
+  }
+
+  if (evt.keyCode === 9) {
+
+    if (evt.shiftKey) {
+
+      if (document.activeElement === firstFocusableElem) {
+        evt.preventDefault();
+        lastFocusableElem.focus();
+      }
+
+    } else {
+      if (document.activeElement === lastFocusableElem) {
+        evt.preventDefault();
+        firstFocusableElem.focus();
+      }
+    }
   }
 };
 
@@ -20,6 +43,8 @@ const closeModal = () => {
   document.body.style.overflow = 'auto';
   modal.dataset.isOpen = false;
 
+  lastFocusedElement.focus();
+
   btnClose.removeEventListener('click', closeModal);
   document.removeEventListener('keydown', keydownDocument);
   modal.removeEventListener('click', onClickOverlay);
@@ -29,8 +54,8 @@ const openModal = () => {
   document.body.style.overflow = 'hidden';
   modal.dataset.isOpen = true;
 
-  const inputName = modal.querySelector('#request-call-name');
-  inputName.focus();
+  lastFocusedElement = document.activeElement;
+  focusableElements.find((elem) => elem.id === 'request-call-name').focus();
 
   btnClose.addEventListener('click', closeModal);
   document.addEventListener('keydown', keydownDocument);
